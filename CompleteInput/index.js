6c1d5d90e479462a62913ed88a27d003102b1478
Amir Titelman <amirti7@gmail.com>
2021-01-14 19:19:20 +0200

    change 1

--- CompleteInput/index.js
+++ CompleteInput/index.js
@@ -1,12 +1,24 @@
 
-const api = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
-/* 
-  In order to find a pattern in a string you will mostly want to use regex and string.match method.
-  You can create a new regex with a custom string by using the RegExp constructor and apply to it the g an i flags,
-  g for global and i in order to ignore case. 
-  Example :
-    const string = '#$)($#)(hey)#$)(@02)';
-    const patternToFind = new RegExp('hey','gi');
-    return string.match(patternToFine) // true;
-*/
+const api = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+const CitiesArr = [];
+fetch(api)
+  .then(api2 => api2.json())
+  .then(data => cities.push(...data));
+
+
+function FindSimillarWord(WordToFind,CitiesArr){
+  CitiesArr.filter(MatchCities=>{
+    const value= new RegExp(WordToFind,'gi');
+    return(MatchCities.city.match(value)||MatchCities.state.match(value))});
+  }
+
+  function ShowLiveMatches(Input){
+    let Word = Input.value;
+    let MatchesArr = FindSimillarCity(Word,CitiesArr);
+   
+  
+  }
+
+  const Input = document.querySelector('.search').addEventListener('change',ShowLiveMatches);
+ 
